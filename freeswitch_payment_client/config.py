2a23c4f50ff207978943ec6c385b4d67c17dfc52
Linus Mixson <linus@opensettlenet.com>
2023-11-29 16:33:07 -0800

    Remove CLI args

--- freeswitch_payment_client/config.py
+++ freeswitch_payment_client/config.py
@@ -1,35 +1,8 @@
-import argparse
-
 from dynaconf import Dynaconf
-
-parser = argparse.ArgumentParser(description="CLI tool options")
-parser.add_argument(
-    "--esl-url", help="URL for the ESL service.", type=str, required=False
-)
-parser.add_argument(
-    "--esl-port", help="Port for the ESL service.", type=int, required=False
-)
-parser.add_argument(
-    "--esl-password", help="Password for the ESL service.", type=str, required=False
-)
-parser.add_argument(
-    "--redis-url", help="URL for the Redis service.", type=str, required=False
-)
-parser.add_argument(
-    "--payment-wallet", help="Wallet for payments.", type=str, required=False
-)
-parser.add_argument(
-    "--payer-key", help="Key for payment wallet.", type=str, required=False
-)
-
-
-args = parser.parse_args()
-
 
 settings = Dynaconf(
     envvar_prefix="FREESWITCH_PAYMENT_CLIENT",
     settings_files=['settings.yaml', '.secrets.yaml'],
-    **vars(args)
 )
 
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
